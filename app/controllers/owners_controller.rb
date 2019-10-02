c106f7bb7040535d77bd6dcd75c93ae79f3ef6a0
scrabill <shannon.crabill@gmail.com>
2019-10-02 17:45:54 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,14 +6,27 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do
+    ####### bug fix
+      if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+      end
+      #######
+
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
 
@@ -23,6 +36,10 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-
+    # binding.pry
+    @owner = Owner.all.find_by_id(params[:id])
+    # @owner[name] = params[:name]
+    @owner[:name] = params[:owner][:name]
+    @owner.save
   end
 end
