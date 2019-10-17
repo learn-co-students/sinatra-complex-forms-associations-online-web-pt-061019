d4e81b6171a85a9c57198bf3e53ce494eb2b56c3
ZumbaIdol <david.a.foote@gmail.com>
2019-10-17 15:08:06 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,7 +33,16 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
+    redirect to "pets/#{@pet.id}"
+  end
 
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
